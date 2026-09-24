66de3698a6892ae458d47df5618b1835a5908145
carlosteroll <carlosjesusterolmartin@gmail.com>
2026-09-24 21:15:43 +0200

    app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ try:
                 """
                 
                 response = client.models.generate_content(
-                    model='gemini-2.5-flash',
+                    model='gemini-1.5-flash',
                     contents=prompt,
                 )
                 st.write(response.text)
