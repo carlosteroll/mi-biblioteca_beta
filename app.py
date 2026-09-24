58fc69e8dabf93f0662b8e610c8efd72faf18aa6
carlosteroll <carlosjesusterolmartin@gmail.com>
2026-09-25 01:57:18 +0200

    app.py

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ try:
                 try:
                     with st.spinner("Buscando en la estantería... 📖"):
                         response = client.models.generate_content(
-                            model='gemini-3.8-flash ',
+                            model='gemini-3.8-flash',
                             contents=prompt,
                         )
                         
